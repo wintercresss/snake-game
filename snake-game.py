17a9e62479a9ff3218826701805e94b1059d5a38
wintercresss <135405194+wintercresss@users.noreply.github.com>
2023-07-03 21:23:24 -0400

    Made snake unable to reverse into itself

--- snake-game.py
+++ snake-game.py
@@ -102,13 +102,17 @@ while True:
             main_game.update()
         if event.type == pygame.KEYDOWN: # check for input on keyboard
             if event.key == pygame.K_UP: # change direction depending on input
-                main_game.snake.direction = Vector2(0, -1)
+                if main_game.snake.direction.y != 1: # make snake unable to reverse into itself
+                    main_game.snake.direction = Vector2(0, -1)
             if event.key == pygame.K_DOWN:
-                main_game.snake.direction = Vector2(0, 1)
+                if main_game.snake.direction.y != -1:
+                    main_game.snake.direction = Vector2(0, 1)
             if event.key == pygame.K_LEFT:
-                main_game.snake.direction = Vector2(-1, 0)
+                if main_game.snake.direction.x != 1:
+                    main_game.snake.direction = Vector2(-1, 0)
             if event.key == pygame.K_RIGHT:
-                main_game.snake.direction = Vector2(1, 0)
+                if main_game.snake.direction.x != -1:
+                    main_game.snake.direction = Vector2(1, 0)
 
 
     screen.fill((175, 215, 70)) # green background color
