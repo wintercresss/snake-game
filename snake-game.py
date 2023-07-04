0b06350dcbe83fe537d4797ff313080ce1f72144
wintercresss <135405194+wintercresss@users.noreply.github.com>
2023-07-03 20:39:42 -0400

    Created logic to move snake

--- snake-game.py
+++ snake-game.py
@@ -9,20 +9,26 @@ class FRUIT:
         self.pos = Vector2(self.x, self.y)
 
     
-    def draw_fruit(self): # create and draw rectangle
+    def draw_fruit(self): # create and draw fruit
         fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size) # x, y, width, height
         pygame.draw.rect(screen, (126, 166, 114), fruit_rect) #surface, color, rectangle (temp color for now)
 
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+        self.direction = Vector2(1, 0) # temporary direction (moving to right), add player input later
     
     def draw_snake(self):
         for block in self.body: # create rectangle from position and draw
             x_pos = int(block.x * cell_size)
             y_pos = int(block.y * cell_size)
             snake_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
-            pygame.draw.rect(screen, (183, 191, 122), snake_rect)
+            pygame.draw.rect(screen, (183, 111, 122), snake_rect)
+    
+    def move_snake(self):
+        body_copy = self.body[:-1] # copies every item in the list except for the last one
+        body_copy.insert(0, body_copy[0] + self.direction) # location of new head
+        self.body = body_copy[:] # update body to new list
 
 
 
@@ -38,6 +44,7 @@ clock = pygame.time.Clock()
 running = True
 
 fruit = FRUIT()
+snake = SNAKE()
 
 
 while running:
@@ -46,7 +53,7 @@ while running:
             running = False
     screen.fill((175, 215, 70)) # green background color
     fruit.draw_fruit()
-
+    snake.draw_snake()
     
         
     pygame.display.update()
