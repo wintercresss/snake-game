1420e9c1223d939675414fae32375a145a864aa0
wintercresss <135405194+wintercresss@users.noreply.github.com>
2023-07-03 21:08:59 -0400

    Wrote collision logic to add new apple and increase snake length

--- snake-game.py
+++ snake-game.py
@@ -4,19 +4,24 @@ from pygame.math import Vector2
 
 class FRUIT:
     def __init__(self):  # create x and y position
-        self.x = random.randint(0, cell_number - 1)
-        self.y = random.randint(0, cell_number - 1)
-        self.pos = Vector2(self.x, self.y)
+        self.randomize()
 
     
     def draw_fruit(self): # create and draw fruit
         fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size) # x, y, width, height
         pygame.draw.rect(screen, (126, 166, 114), fruit_rect) #surface, color, rectangle (temp color for now)
+    
+    def randomize(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+
 
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
         self.direction = Vector2(1, 0) # temporary direction (moving to right), add player input later
+        self.new_block = False
     
     def draw_snake(self):
         for block in self.body: # create rectangle from position and draw
@@ -26,9 +31,20 @@ class SNAKE:
             pygame.draw.rect(screen, (183, 111, 122), snake_rect)
     
     def move_snake(self):
-        body_copy = self.body[:-1] # copies every item in the list except for the last one
-        body_copy.insert(0, body_copy[0] + self.direction) # location of new head
-        self.body = body_copy[:] # update body to new list
+        if self.new_block:
+            body_copy = self.body[:] # copy every block (since new block is being added)
+            body_copy.insert(0, body_copy[0] + self.direction) # location of new head
+            self.body = body_copy[:] # update body to new list
+            self.new_block = False # done with adding new block
+        
+        else:
+            body_copy = self.body[:-1] # copies every item in the list except for the last one
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+    
+    def add_block(self):
+        self.new_block = True
+
 
 class MAIN:
     def __init__(self):
@@ -37,10 +53,16 @@ class MAIN:
     
     def update(self):
         self.snake.move_snake()
+        self.check_collision()
 
     def draw_elements(self):
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+    
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]: # if the head of the snake collides with the fruit
+            self.fruit.randomize() # make new fruit spawn in new location
+            self.snake.add_block()
 
 
 pygame.init()
